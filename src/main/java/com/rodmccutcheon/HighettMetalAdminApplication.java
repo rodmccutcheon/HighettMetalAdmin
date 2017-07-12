ae69ee0b364bf1f1bfb5f70a96bb2988a731a4cd
Rod McCutcheon <rodmccutcheon@gmail.com>
2017-07-12 17:07:38 +1000

    Initial commit

---
+++ src/main/java/com/rodmccutcheon/HighettMetalAdminApplication.java
@@ -0,0 +1,12 @@
+package com.rodmccutcheon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HighettMetalAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HighettMetalAdminApplication.class, args);
+	}
+}
---
+++ src/test/java/com/rodmccutcheon/HighettMetalAdminApplicationTests.java
@@ -0,0 +1,16 @@
+package com.rodmccutcheon;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HighettMetalAdminApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
