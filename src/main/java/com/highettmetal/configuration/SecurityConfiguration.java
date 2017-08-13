ef4c3b0f2a58ab280f1b8c4a4c4fb2bce06a926e
Rod McCutcheon <rodmccutcheon@gmail.com>
2017-08-13 11:12:01 +1000

    Adding configuration

--- src/main/java/com/highettmetal/configuration/SecurityConfiguration.java
+++ src/main/java/com/highettmetal/configuration/SecurityConfiguration.java
@@ -12,7 +12,13 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity httpSecurity) throws Exception {
-        httpSecurity.authorizeRequests().antMatchers("/").permitAll();
+        httpSecurity
+                .authorizeRequests()
+                    .antMatchers("/").permitAll()
+                    .antMatchers("/console/**").permitAll();
+
+        httpSecurity.csrf().disable();
+        httpSecurity.headers().frameOptions().disable();;
     }
 
 }
